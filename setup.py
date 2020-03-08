fbfc4b763042c0aea2525322ff3b8f5b690eb6ec
Tim Richards <richards@cs.umass.edu>
2020-03-08 13:52:19 -0400

    udpate again

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 import setuptools
 long_description = 'My first pip library'
 setuptools.setup(
-     name='testpackages',  
-     version='0.1',
-     author="Tim Richards",
-     author_email="richards@cs.umass.edu",
-     description="Test Package",
-     packages=["info121"],
-     long_description=long_description,
-     # long_description_content_type="text/markdown",
-     url="https://github.com/timdrichards/info121",
-     packages=setuptools.find_packages(),
+    name='testpackages',
+    url="https://github.com/timdrichards/info121",
+    author="Tim Richards",
+    author_email="richards@cs.umass.edu",
+    packages=["info121"],
+    version='0.1',
+    license='MIT',
+    description="Test Package",
+    #long_description=long_description,
+    #long_description_content_type="text/markdown",
+    #packages=setuptools.find_packages(),
  )
