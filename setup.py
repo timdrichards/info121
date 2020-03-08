6f03b457e74ee99b0c355f6716293d079b0ca01f
Tim Richards <richards@cs.umass.edu>
2020-03-08 13:47:48 -0400

    update

--- setup.py
+++ setup.py
@@ -6,8 +6,9 @@ setuptools.setup(
      author="Tim Richards",
      author_email="richards@cs.umass.edu",
      description="Test Package",
+     packages=["info121"],
      long_description=long_description,
-   long_description_content_type="text/markdown",
+     # long_description_content_type="text/markdown",
      url="https://github.com/timdrichards/info121",
      packages=setuptools.find_packages(),
  )
