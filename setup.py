ece9f48de92bdaf94375530a2c2e612043c82c25
Tim Richards <richards@cs.umass.edu>
2020-03-09 13:37:00 -0400

    another update

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 import setuptools
 long_description = 'My first pip library'
 setuptools.setup(
-    name='testpackages',
+    name='info121',
     url="https://github.com/timdrichards/info121",
     author="Tim Richards",
     author_email="richards@cs.umass.edu",
-    packages=["info121"],
+    packages=find_packages(),
     version='0.1',
     license='MIT',
     description="Test Package",
