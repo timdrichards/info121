ab60bdcb546eb71ab1d36890985d29d3bdce7190
Tim Richards <richards@cs.umass.edu>
2020-03-09 13:40:36 -0400

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     url="https://github.com/timdrichards/info121",
     author="Tim Richards",
     author_email="richards@cs.umass.edu",
-    packages=find_packages(),
+    packages=['info121'],
     version='0.1',
     license='MIT',
     description="Test Package",
